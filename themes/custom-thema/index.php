5ffdb165feb6dae76abb356a155e043488c458a0
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2025-01-22 11:48:50 +0100

    Side posts

--- themes/custom-thema/index.php
+++ themes/custom-thema/index.php
@@ -8,6 +8,52 @@
 get_header(); // Include the header template part.
 ?>
 <div id="main" role="main">
+    <div class="side-posts">
+        <!-- Intro -->
+        <div class="intro">
+            <header>
+                <h2>GreenTech Solutions</h2>
+                <p>Lorem ipsum</p>
+            </header>
+        </div>
+
+        <!-- Mini Posts -->
+        <div>
+            <div class="mini-posts">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <article class="mini-post">
+                        <header>
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                            <time class="published" datetime="<?php get_the_date() ?>"><?php echo get_the_date() ?></time>
+                            <a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+                        </header>
+                        <a href="single.html" class="image"><?php getTheFirstImage(); ?></a>
+                    </article>
+                <?php endwhile; ?>
+            </div>
+        </div>
+        
+        <!-- About -->
+        <div class="blurb">
+            <h2>About</h2>
+            <p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod amet placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at phasellus sed ultricies.</p>
+            <ul class="actions">
+                <li><a href="#" class="button">Learn More</a></li>
+            </ul>
+        </div>
+
+        <!-- Footer -->
+        <div id="footer">
+            <ul class="icons">
+                <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                <li><a href="#" class="icon solid fa-rss"><span class="label">RSS</span></a></li>
+                <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+            </ul>
+            <p class="copyright">&copy; Untitled.</p>
+        </div>
+    </div>
     <div class="posts-list">
         <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
